c0420cec947dcda4677a3e982a6afd7a6479ad4b
unknown <doris.bednarska@gmail.com>
2017-02-04 15:23:49 +0100

    Added new class with scanner options. Added grade system fot students

--- src/zajecia/czwarte/ZadaniaZeScannerem.java
+++ src/zajecia/czwarte/ZadaniaZeScannerem.java
@@ -1,7 +1,136 @@
 package zajecia.czwarte;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
 /**
  * Created by RENT on 2017-02-04.
  */
 public class ZadaniaZeScannerem {
+    public static void main(String[] args) {
+        System.out.println("Grade system 2000 z.o.o");
+        double avg = avgOfGrades();
+        System.out.println("Avg of your grades is: " + avg);
+    }
+
+    public static double avgOfGrades() {
+        int size = getNumberFromUser("Insert number of grades: ");
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = getNumberFromUser("Insert " + (i + 1) + " grade: ");
+        }
+        return Zajecia4.avg(array);
+    }
+
+    public static void compareThreeNumbers() {
+        int a = getNumberFromUser("Insert first number");
+        int b = getNumberFromUser("Insert second number");
+        int c = getNumberFromUser("Insert third number");
+        //pobieramy od usera 3 liczby
+        System.out.println("Max: " + max(a, b, c));
+        System.out.println("Min: " + min(a, b, c));
+        //wyswietlamy największą i najmniejszą
+    }
+
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+//    public static int min (int a, int b, int c) {
+//        if (a <=b && a<= c) {
+//            return a;
+//        } else if ( b <= a && b<=c){
+//            return b;
+//        } else {
+//            return c;
+//        }
+//    }
+
+    public static int min(int a, int b, int c) {
+        int[] array = {a, b, c};
+        return min(array);
+    }
+
+    public static int max(int a, int b, int c) {
+        int[] array = {a, b, c};
+        return max(array);
+    }
+
+    public static void listEvenNumbersFromUser() {
+        int number = getNumberFromUser();
+        Zajecia4.printEvenLessThan(number);
+        //pobieramy liczbę
+        //wyświetlamy tylko parzyste mniejsze od...
+    }
+
+//    public static int max (int a,int b, int c) {
+//        if (a >= b && a>=c) {
+//            return a;
+//        } else if ( b>= a && b>=c){
+//            return b;
+//        } else {
+//            return c;
+//        }
+//    }
+
+    public static void listNumbersFromUser() {
+        int number = getNumberFromUser();
+        Zajecia4.printLessThan(number);
+    }
+
+    public static int getNumberFromUser() {
+        return getNumberFromUser("Please insert the number: ");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please insert the number: ");
+//        return scanner.nextInt();
+    }
+
+    public static int getNumberFromUser(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        return scanner.nextInt();
+    }
+
+
+    public static void checNumberFromUser() {
+        int numberFromUser = getNumberFromUser();
+        if (Zajecia4.isEven(numberFromUser)) {
+            System.out.println("Your number is even");
+        } else {
+            System.out.println("Your number is odd");
+        }
+        //definiujemy scanner
+        //czytamy od usera liczbe
+        //sprawdzamy czy liczba jest parzysta
+        //jeżeli tak to wiadomość
+        //jeżeli nie to wiadomość
+
+    }
+
+    public static void scannerExample() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert your name:");
+        String name = scanner.nextLine();
+        System.out.println("Please insert your last name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Your name is " + name + " " + lastName);
+    }
 }
--- src/zajecia/czwarte/Zajecia4.java
+++ src/zajecia/czwarte/Zajecia4.java
@@ -1,4 +1,4 @@
-package zajecia;
+package zajecia.czwarte;
 
 import java.util.Scanner;
 
@@ -11,54 +11,42 @@ public class Zajecia4 {
         statistics(array);
     }
 
-    //checkAge();
-    //isEven();
-    //celsiustofahrenheit();
-    //showArray1();
-    //showArray2();
-//        oddArray();
-//
-    //scannerExample();
-//}
-
-    public static void checkAge() {
-        int age = 5;
-        if (age >= 18) {
-            System.out.println("Ok, masz 18 lat");
-        } else {
-            System.out.println("Uwaga! Nie masz 18 lat");
+    public static void printLessThan(int number) {
+        for (int i = 0; i <= number; i++) {
+            System.out.println(i);
         }
     }
 
-    public static void showArray1() {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+    public static void printEvenLessThan(int number) {
+        for (int i = 0; i < number; i += 2) {
+            System.out.println(i);
         }
     }
 
-    public static void showArray2() {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                int num = array[i] * array[i];
-                System.out.println(num);
-            }
+    public static int sumOfLessThan(int number) {
+        int sum = 0;
+        for (int i = 0; i <= number; i++) {
+            sum += i;
         }
+        return sum;
     }
 
-    public static void oddArray() {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0) {
-                System.out.println(array[i]);
-            }
+    public static int productOfLessThan(int number) {
+        int product = 1;
+        for (int i = 1; i <= number; i++) {
+            product *= i;
         }
+        return product;
     }
 
-    public static double celsiustofahrenheit(int value) {
+    public static double celsiusToFahrenheit(int value) {
         return (value * (9.0 / 5.0)) + 32;
     }
+
+    public static double fahrenheitToCelsius(int value) {
+        return (value - 32) / 1.8;
+    }
+
 
     public static void statistics(int[] array) {
         System.out.println("Avg of elements is: " + avg(array));
@@ -66,72 +54,59 @@ public class Zajecia4 {
         System.out.println("Product of elements is: " + productOf(array));
         System.out.println("Number of even elements is: " + numberOfEven(array));
         System.out.println("Number of odd elements is: " + numberOfOdd(array));
-//          1.srednia elementów
-//          2.suma elementow
-//          3.iloczyn elementow
-//          4.ilosc parzystych elementow
-//          5.ilosc niepatrzystych elementow
+        //1. srednia elementow
+        //2. suma elementow
+        //3. iloczyn elementow
+        //4. ilosc parzystych elementow
+        //5. ilosc nieparzystych elementow
     }
 
-    public static int numberOfOdd(int[] array){
+    public static int numberOfOdd(int[] array) {
         return array.length - numberOfEven(array);
     }
 
     public static int numberOfEven(int[] array) {
-        int counter =0;
+        int counter = 0;
         for (int i = 0; i < array.length; i++) {
-            if (isEven(array[i])){
+            if (isEven(array[i])) {
                 counter++;
             }
         }
         return counter;
     }
 
-    //metoda sprawdzająca czy dana liczba jest parzysta
     public static boolean isEven(int number) {
-        if (number % 2 == 0){
+        if (number % 2 == 0) {
             return true;
-    }   else{
-        return false;
-        }
-    }
-
-      public static double avg(int[] array){
-          double avg = (double)sumOf(array)/array.length; //pierwsza zmienna definiujemy jako double, żeby nie obcięło miejsc po przecinku (rzutowanie)
-          return avg;
-      }
-      public static int productOf(int[]array){
-          int product =1;
-          for(int i=0;i<array.length;i++){
-              product += array[i];
-          }
-          return product;
-      }
-
-      public static int sumOf(int[] array){
-          int sum=0;
-          for (int i=0;i<array.length;i++) { //jeżeli robimy coś na wszystkich elementach tablicy}
-          sum=sum+array[i]; //sum+=array[i];
-          }
-          return sum;
-      }
-
-    public static void isEven() {
-        int number = 5;
-        if (number %2==0){
-            System.out.println("ta liczba jest parzysta");
         } else {
-            System.out.println("ta liczba NIE JEST przysta");
+            return false;
         }
     }
-    public static void scannerExample(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please insert your name:");
-        String name = scanner.nextLine();
-        System.out.println("Please insert your last name:");
-        String lastName = scanner.nextLine();
 
-        System.out.println("Your name is " + name + " " + lastName);
+    public static double avg(int[] array) {
+        double avg = (double) sumOf(array) / array.length;
+        return avg;
     }
 
+    public static int productOf(int[] array) {
+        int product = 1;
+        for (int i = 0; i < array.length; i++) {
+            product *= array[i];
+        }
+        return product;
+    }
+
+    public static int sumOf(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+//
+//    public static int sumOfLessThan(int number) {
+//
+//    }
 }
+
+//printf() -> System.out.println("Hello World");
