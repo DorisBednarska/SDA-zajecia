96aa4c12ed4aed974262e049be1c9edfce21ef7b
unknown <doris.bednarska@gmail.com>
2017-02-06 20:56:49 +0100

    Added game

--- src/zajecia/Zajecia5/poniedzialek.java
+++ src/zajecia/Zajecia5/poniedzialek.java
@@ -1,12 +1,18 @@
 package zajecia.Zajecia5;
 
+import zajecia.czwarte.ZadaniaZeScannerem;
+
+import java.util.Random;
+
 /**
  * Created by RENT on 2017-02-06.
  */
 public class poniedzialek {
     public static void main(String[] args) {
-        evenMatrix(4);
-
+        Random random = new Random();
+        int iloscKrokowDoZwyciestwa = game(random.nextInt(100));
+        System.out.println("Koniec gry");
+        System.out.println("Wygrales w " + iloscKrokowDoZwyciestwa + " krokach");
     }
 
     public static void triangle(int number) {
@@ -70,5 +76,69 @@ public class poniedzialek {
             }
             System.out.println();
         }
+    }
+
+    public static int sumOfNumbersFromUser() {
+        int sum = 0;
+        boolean flag = true;
+        while (flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser("Insert next number: ");
+            if (numberFromUser != 0) {
+                sum += numberFromUser;
+            } else {
+                flag = false;
+            }
+        }
+        return sum;
+    }
+
+    public static int[] arrayOfNumbersFromUser() {
+        int[] tmparray = new int[100];
+        int size = 0;
+        boolean flag = true;
+        while (flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser();
+            if (numberFromUser != 0) {
+                tmparray[size] = numberFromUser;
+                size++;
+            } else {
+                flag = false;
+            }
+        }
+        return revriteArray(tmparray, size);
+    }
+
+    public static int[] revriteArray(int[] bigArray, int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = bigArray[i];
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static int game(int number) {
+        int[] tmparray = new int[100];
+        int counter = 0;
+        int size = 0;
+        boolean flag = true;
+        while (flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser();
+            counter++;
+            if (numberFromUser == number) {
+                System.out.println("Gratulacje!");
+                flag = false;
+            } else if (numberFromUser > number) {
+                System.out.println("Twoja liczba jest za wysoka");
+            } else {
+                System.out.println("Twoja liczba jest za niska");
+            }
+        }
+        return counter;
     }
 }
