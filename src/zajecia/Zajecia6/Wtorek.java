5e990b360008520087e957387a95d92a432dc7ed
unknown <doris.bednarska@gmail.com>
2017-02-07 19:44:31 +0100

    Added Zajecia6 class with calculator method

---
+++ src/zajecia/Zajecia6/Wtorek.java
@@ -0,0 +1,93 @@
+package zajecia.Zajecia6;
+
+import zajecia.czwarte.ZadaniaZeScannerem;
+
+/**
+ * Created by RENT on 2017-02-07.
+ */
+
+public class Wtorek {
+    public static void main(String[] args) {
+//        int numberOfElements = avgUntil(4);
+//        System.out.println("Podano " + numberOfElements + " liczb.");
+        calculator();
+    }
+
+    public static int sumUntil(int sum) {
+        int tmpSum = 0;
+        int counter = 0;
+        boolean flag = true;
+        while (flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser();
+            tmpSum += numberFromUser;
+            counter++;
+            System.out.println("tmpSum: " + tmpSum);
+            if (tmpSum > sum) {
+                flag = false;
+            }
+        }
+        return counter;
+    }
+
+    public static int avgUntil(int avg) {
+        boolean flag = true;
+        int sum = 0;
+        int counter = 0;
+        while (flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser();
+            sum += numberFromUser;
+            counter++;
+            if ((double) sum / counter > avg) {
+                flag = false;
+            }
+        }
+        return counter;
+    }
+
+    public static void calculator() {
+        boolean flag = true;
+        while (flag) {
+            System.out.println("1. Dodawanie");
+            System.out.println("2. Odejmowanie");
+            System.out.println("3. Mnożenie");
+            System.out.println("4. Dzielenie");
+            System.out.println("0. Koniec");
+            int nrDzialania = ZadaniaZeScannerem.getNumberFromUser("Podaj nr dzialania");
+            int firstNumber = 0;
+            int secondNumber = 0;
+            if (nrDzialania > 0 && nrDzialania < 5) {
+                firstNumber = ZadaniaZeScannerem.getNumberFromUser("Podaj pierwsza liczbe: ");
+                secondNumber = ZadaniaZeScannerem.getNumberFromUser("Podaj druga liczbe: ");
+            }
+            switch (nrDzialania) {
+                case 1:
+                    System.out.println(firstNumber + " + " + secondNumber + " = " + (firstNumber+secondNumber));
+                    break;
+                case 2:
+                    System.out.println(firstNumber + " - " + secondNumber + " = " + (firstNumber-secondNumber));
+                    break;
+                case 3:
+                    System.out.println(firstNumber + " * " + secondNumber + " = " + (firstNumber*secondNumber));
+                    break;
+                case 4:
+                    if (secondNumber == 0){
+                        System.out.println("Nie dziel przez 0!");
+                    }else {
+                        System.out.println(firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber));
+                    }
+                    break;
+                case 0:
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Podano niepoprawna operacje");
+            }
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+        }
+
+    }
+}
